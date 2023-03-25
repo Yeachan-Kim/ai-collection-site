542be9780575983b2764e996c482bc6019e19e65
Yea-chan Kim <ghaeun1009@gmail.com>
2023-03-25 23:28:46 +0900

    set basic server

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.listen(PORT, () => {
+  console.log(` ${PORT} 포트에서 서버 열림`);
+});
